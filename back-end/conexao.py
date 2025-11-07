6bd72c07466e2c887ac050a2c73f932c8994b8cd
Rayssa <rayssa.rebeca955@gmail.com>
2025-11-07 19:45:25 -0300

    estrutura + conexão estabelecida

---
+++ back-end/conexao.py
@@ -0,0 +1,28 @@
+#pip install fastapi uvicorni 
+# pip install psycopg2
+#pip install dotenv 
+
+import psycopg2
+from dotenv import load_dotenv
+import os 
+
+#Carregar .env
+
+load_dotenv()
+
+def conector():
+    try:
+        conexao = psycopg2.connect(
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT") 
+        )
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão {erro}")
+        return None, None
+
+conector()
